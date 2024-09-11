5aa211171203db2cd029fa5be1e739a0cd2f41da
Raul <42917076+elRaulito@users.noreply.github.com>
2024-09-11 12:21:15 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,3 @@
-// next.config.mjs
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
@@ -7,21 +5,9 @@ const nextConfig = {
     config.experiments = {
       asyncWebAssembly: true,
       layers: true,
-      topLevelAwait: true
     };
-
-    // Add a rule for handling .wasm files
-    config.module.rules.push({
-      test: /\.wasm$/,
-      type: 'asset/resource', // Handles .wasm as an asset/resource
-    });
-
-    // Ensure correct MIME type for WASM files
-    config.output.webassemblyModuleFilename = 'static/wasm/[modulehash].wasm';
-
     return config;
   },
-  output: 'export',  // Enable static export
 };
 
 export default nextConfig;
